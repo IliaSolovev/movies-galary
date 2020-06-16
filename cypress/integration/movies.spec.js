6cc5a35f0da8e8433bcfa0ff12df4a61bd080cb3
tregdor <49729985+tregdor@users.noreply.github.com>
2020-06-16 13:31:24 +0300

    fix cypress bug after rewrote button component

--- cypress/integration/movies.spec.js
+++ cypress/integration/movies.spec.js
@@ -18,22 +18,22 @@ describe('select movie', () => {
 describe('filter ', () => {
   it('filter should saved', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('button').contains('release date').click().should('have.class', 'components-MovieSortFilter-FilterButton-filterButton-module__button_active--zrPCE');
+    cy.get('button').contains('release date').click().should('have.class', 'components-Button---2Gwqy');
     cy.get('input').type('drama').should('have.value', 'drama');
     cy.get('button').contains('search').click();
     cy.get('a[href*="/movie"]').first().click();
     cy.get('button').contains('search').click();
-    cy.get('button').contains('release date').should('have.class', 'components-MovieSortFilter-FilterButton-filterButton-module__button_active--zrPCE');
+    cy.get('button').contains('release date').should('have.class', 'components-Button---2Gwqy');
   });
 });
 describe('search type', () => {
   it('filter should saved', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('button').contains('title').click().should('have.class', 'components-SearchMovieForm-SearchMovieFilters-SearchMovieFiltersButton-searchMovieFiltersButton-module__button_active--wAITI');
+    cy.get('button').contains('title').click().should('have.class', 'components-Button---3lHdr'); // active class
     cy.get('input').type('mad').should('have.value', 'mad');
     cy.get('button').contains('search').click();
     cy.get('a[href*="/movie"]').first().click();
     cy.get('button').contains('search').click();
-    cy.get('button').contains('title').should('have.class', 'components-SearchMovieForm-SearchMovieFilters-SearchMovieFiltersButton-searchMovieFiltersButton-module__button_active--wAITI');
+    cy.get('button').contains('title').should('have.class', 'components-Button---3lHdr'); // active class
   });
 });
